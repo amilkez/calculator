1a7bfeddb13a1576b902bcc00295ed51e2ed0362
angel <angelhidalgomb@gmail.com>
2022-05-03 13:32:57 -0500

    Change functionality to use a floating point

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+const currentNumberEle = document.querySelector(".current-number");
+const lastOperation = document.querySelector(".last-operation");
+const numbers = document.querySelectorAll(".num");
+const operators = document.querySelectorAll("operator");
+const point = document.querySelector(".point");
+const ac = document.querySelector(".ac");
+const del = document.querySelector(".del");
+const clear = document.querySelector(".clear");
+const equal = document.querySelector(".equal");
+
 const add = (num1, num2) => {
 	return num1 + num2;
 };
@@ -16,19 +26,33 @@ const multiply = (num1, num2) => {
 
 const operate = (operator, num1, num2) => {
 	switch (operator) {
-		case "add":
 		case "+":
 			return add(num1, num2);
-		case "subtract":
 		case "-":
 			return subtract(num1, num2);
-		case "divide":
 		case "/":
 			return divide(num1, num2);
-		case "multiply":
 		case "*":
 			return multiply(num1, num2);
 		default:
 			return "ERROR";
 	}
 };
+
+let displayNumber = "";
+
+numbers.forEach((num) => {
+	num.addEventListener("click", () => {
+		displayNumber += num.textContent;
+		currentNumberEle.textContent = displayNumber;
+	});
+});
+
+point.addEventListener(
+	"click",
+	() => {
+		displayNumber += ".";
+		currentNumberEle.textContent = displayNumber;
+	},
+	{ once: true }
+);
