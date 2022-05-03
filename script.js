a4cab922727b4a2b2a39a4cb2675d3c61a92a941
angel <angelhidalgomb@gmail.com>
2022-05-02 20:42:32 -0500

    Create main function for all operations

--- script.js
+++ script.js
@@ -13,3 +13,22 @@ const divide = (num1, num2) => {
 const multiply = (num1, num2) => {
 	return num1 * num2;
 };
+
+const operate = (operator, num1, num2) => {
+	switch (operator) {
+		case "add":
+		case "+":
+			return add(num1, num2);
+		case "subtract":
+		case "-":
+			return subtract(num1, num2);
+		case "divide":
+		case "/":
+			return divide(num1, num2);
+		case "multiply":
+		case "*":
+			return multiply(num1, num2);
+		default:
+			return "ERROR";
+	}
+};
